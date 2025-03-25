7038b5aa1df834295662acffb884e796b57bedf6
Just_Val <valentin.jardon@protonmail.com>
2025-03-25 19:55:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+function newFeed() {
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
